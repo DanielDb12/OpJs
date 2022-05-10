395a89c7b2ff3a1d50e04bcf15a2ec49a9e584ab
Daniel <danielduarte1991gmail.com>
2022-05-10 11:28:31 -0400

    modificacion

--- 02-Sintax-variables-preservada/ejercicio/js/ejericio2.js
+++ 02-Sintax-variables-preservada/ejercicio/js/ejericio2.js
@@ -1,13 +1,13 @@
-let cadena = "hola"
-
-
-
-function texto() {
-  let cadena = 'hello'
- console.log(cadena) 
+const lista = [
+  "Daniel",
+   30,
+   true,
+   new Date(1991,3,12),
+  {
+titulo: "El monje que vendió su Ferrari",
+      autor: "Robin S. Sharma",
+      fecha: new Date(1996, 0, 1),
+      url: "https://www.amazon.es/dp/B0062XBS32/"
+  },
   
-}
-
-texto()
-
-console.log(cadena)
+]
