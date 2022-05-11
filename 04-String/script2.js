32bde8cdd8d170384cc216f8958a70e90c84b544
Daniel <danielduarte1991gmail.com>
2022-05-11 10:03:32 -0400

    modif

--- 04-String/script2.js
+++ 04-String/script2.js
@@ -1,2 +1,10 @@
-const mayusculas = "esta es una historia real";
-console.log(mayusculas.toUpperCase());
+const Nombre = "Daniel"
+const Apellido = "Duarte"
+const estudiante = Nombre.concat(" ").concat(Apellido),
+  estudianteMayus = estudiante.toUpperCase(),
+  estudianteMinus = estudiante.toLowerCase(),
+  numeroLetras = estudiante.length,
+  firstletter = Nombre[0],
+  lastletter = Apellido[Apellido.length - 1],
+  deleteName = estudiante.replace(/ /g, ""),
+nombreEnEstudiante = estudiante.includes(Nombre)
